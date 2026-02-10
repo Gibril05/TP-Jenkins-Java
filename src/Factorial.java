e3e8ae4b7272501209a2a6412e193e2eedab5dd0
zaoui gibril <gibril_zaoui@univ-artois.fr>
2026-02-10 14:57:03 +0100

    Fix BOM encoding for Java

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,4 +1,4 @@
-﻿public class Factorial {
+public class Factorial {
     public static long factorial(int n) {
         if (n < 0) throw new IllegalArgumentException("Le nombre doit être positif!");
         if (n == 0 || n == 1) return 1;
